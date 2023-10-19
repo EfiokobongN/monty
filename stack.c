829e4924b8368cd79c610fe628315e4db36386cc
EfiokobongN <mailpreciousnelson@gmail.com>
2023-10-19 07:32:10 -0700

    alx team project

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - print top
+ * @head: head
+ * @counter: line_number
+ * Return: return nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
